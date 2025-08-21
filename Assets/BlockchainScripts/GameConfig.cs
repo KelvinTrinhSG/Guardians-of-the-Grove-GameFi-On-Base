a909bd1c5121dc15a15bdaa4ede74d8001a09f7a
KelvinTrinhSG <thkien85@Gmail.com>
2025-08-21 19:43:03 +0700

    Update Unity Project
    Update Unity Project

---
+++ Assets/BlockchainScripts/GameConfig.cs
@@ -0,0 +1,12 @@
+﻿public static class GameConfig
+{
+    // Địa chỉ contract ERC721
+    public static readonly string NFTTokenGateContractAddress = "0xd3A03d60Fc56F3Fb43B7A44eb67cc89eB9Ad88c1";
+
+    // Địa chỉ contract ERC721 khác (nếu khác với TokenGate)
+    public static readonly string NFTVIPContractAddress = "0x9451050d7281479ec0D08d65b17D55dD6985aC0a";
+
+    // Địa chỉ contract ERC20 - GEM Token
+    public static readonly string TokenGemContractAddress = "0x79731209Da19635E2A5823764CA04299F17ebA5C";
+
+}
